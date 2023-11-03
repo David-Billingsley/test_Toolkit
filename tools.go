b0868f9c4c63e03047382b57115446e0ee417715
David-Billingsley <5779556+David-Billingsley@users.noreply.github.com>
2023-11-03 15:50:58 -0700

    Update tools.go

--- tools.go
+++ tools.go
@@ -2,7 +2,7 @@ package toolkit
 
 import "crypto/rand"
 
-const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+const randomStringSource = "+-*/"
 
 type Tools struct {
 }
